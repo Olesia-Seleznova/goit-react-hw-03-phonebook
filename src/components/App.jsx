5f26ed7501da1869fc5dc1d6ba63f7fdb3197c92
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2023-09-11 23:08:38 +0300

    apdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForn';
+import { Contacts } from './Contacts/Contacts';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  newContact = data => {
+    const contacts = this.state.contacts;
+    const isDublicate = contacts.some(
+      contact => contact.name.toLowerCase() === data.name.toLowerCase()
+    );
+
+    if (isDublicate) {
+      return alert(`${data.name} is already in contacts!`);
+    }
+
+    const contact = {
+      ...data,
+      id: nanoid(),
+    };
+
+    this.setState(({ contacts }) => {
+      return { contacts: [contact, ...contacts] };
+    });
+  };
+
+  onDelete = contactId => {
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  setFilter = evt => {
+    this.setState({ filter: evt.target.value });
+  };
+
+  filteredNames = () => {
+    const { contacts, filter } = this.state;
+    const filtered = contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return filtered;
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phone book</h1>
+        <ContactForm onSubmit={this.newContact} />
+
+        <h2>Contacts</h2>
+        <Filter onFilter={this.setilter} />
+        <Contacts contacts={this.filteredNames()} onDelete={this.onDelete} />
+      </div>
+    );
+  }
+}
